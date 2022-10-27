48af84ec780e87819f30678ca90f964b7eb085c3
jayumaks <jayumaks@gmail.com>
2022-10-27 21:03:58 +0100

    week 5

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,26 @@
+from email.policy import default
+# from tkinter import CASCADE
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField( max_length=50)
+    last_name = models.CharField( max_length=50)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    Artist = models.ForeignKey(Artiste, on_delete = models.PROTECT)
+    title = models.CharField(max_length=50)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.PositiveIntegerField()
+    artiste_id = models.IntegerField()
+
+class Lyrics(models.Model):
+    Song  = models.ForeignKey(Song, on_delete = models.PROTECT)
+    content = models.TextField()
+    song_id = models.IntegerField()
+
