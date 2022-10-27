816f3f32809cc764f6d1400344bc163287b40a2a
jayumaks <jayumaks@gmail.com>
2022-10-27 22:16:19 +0100

    week 5

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Song, Artiste, Lyric
 
 # Register your models here.
+admin.site.register(Song)
+admin.site.register(Artiste)
+admin.site.register(Lyric)
